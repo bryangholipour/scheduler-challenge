4fc8ec25c25c838885a7b7f75ea158ff4b8897ad
bryangholipour <113397778+bryangholipour@users.noreply.github.com>
2022-10-06 19:41:54 -0700

    finished readme

--- Assets/script.js
+++ Assets/script.js
@@ -1,4 +1,4 @@
-var timeNow= moment().format("H:m A");
+var timeNow= moment().format("hh:m A");
 var dateNow= moment().format("MMM D");
 $("#now").text(timeNow);
 $("#date").text(dateNow);
@@ -13,8 +13,8 @@ $("#date").text(dateNow);
 //     document.getElementsByClassName(".future")
 // }
 
-// if (timeNow >= 9 && timeNow <= 10){
-
+// if (timeNow >= 19 && timeNow <= 20){
+//     document.getElementById("#saveBtn2")= (".present");
 // }
 
 
@@ -37,55 +37,55 @@ $("#date").text(dateNow);
 
 // if (timeNow >= 17 && timeNow <= 18)
 
+// if (timeNow >= 19 && timeNow <=20)
+
+// function saveData() {
+//     var eventText = localStorage.getitem("#eventText")
+// }
 
 
-function saveData() {
-    var eventText = localStorage.getitem("#eventText")
-}
+// var saveBtn2 = document.querySelector("#saveBtn2");
+// var saveBtn3 = document.querySelector("#saveBtn3");
+// var saveBtn4 = document.querySelector("#saveBtn4");
+// var saveBtn5 = document.querySelector("#saveBtn5");
+// var saveBtn6 = document.querySelector("#saveBtn6");
+// var saveBtn7 = document.querySelector("#saveBtn7");
+// var saveBtn8 = document.querySelector("#saveBtn8");
+// var saveBtn9 = document.querySelector("#saveBtn9");
 
+// var clickHandler3 = function (event) {
 
-var saveBtn2 = document.querySelector("#saveBtn2");
-var saveBtn3 = document.querySelector("#saveBtn3");
-var saveBtn4 = document.querySelector("#saveBtn4");
-var saveBtn5 = document.querySelector("#saveBtn5");
-var saveBtn6 = document.querySelector("#saveBtn6");
-var saveBtn7 = document.querySelector("#saveBtn7");
-var saveBtn8 = document.querySelector("#saveBtn8");
-var saveBtn9 = document.querySelector("#saveBtn9");
-
-var clickHandler3 = function (event) {
-
-}
+// }
 
 
 
-saveBtn1.addeventlistener("click" , function(event) {
-    event.preventDefault();
-    var saveBtn1 = document.querySelector("#eventText1").value;
-    localStorage.setItem("eventText1", saveBtn1)
-});
+// saveBtn1.addeventlistener("click" , function(event) {
+//     event.preventDefault();
+//     var saveBtn1 = document.querySelector("#eventText1").value;
+//     localStorage.setItem("eventText1", saveBtn1)
+// });
 
 
-saveBtn2.addeventlistener("click" , function(){
-    localstorage
-})
-saveBtn3.addeventlistener("click" ,clickHandler3 )
-saveBtn4.addeventlistener("click" , )
-saveBtn5.addeventlistener("click" , )
-saveBtn6.addeventlistener("click" , )
-saveBtn7.addeventlistener("click" , )
-saveBtn8.addeventlistener("click" , )
-saveBtn9.addeventlistener("click" , )
+// saveBtn2.addeventlistener("click" , function(){
+//     localstorage
+// })
+// saveBtn3.addeventlistener("click" ,clickHandler3 )
+// saveBtn4.addeventlistener("click" , )
+// saveBtn5.addeventlistener("click" , )
+// saveBtn6.addeventlistener("click" , )
+// saveBtn7.addeventlistener("click" , )
+// saveBtn8.addeventlistener("click" , )
+// saveBtn9.addeventlistener("click" , )
 
-fetchButton.addEventListener('click', getApi);
+// fetchButton.addEventListener('click', getApi);
 
 
-var emailInput = document.querySelector("#email");
-var passwordInput = document.querySelector("#password");
-var signUpButton = document.querySelector("#sign-up");
-var msgDiv = document.querySelector("#msg");
-var userEmailSpan = document.querySelector("#user-email");
-var userPasswordSpan = document.querySelector("#user-password");
+// var emailInput = document.querySelector("#email");
+// var passwordInput = document.querySelector("#password");
+// var signUpButton = document.querySelector("#sign-up");
+// var msgDiv = document.querySelector("#msg");
+// var userEmailSpan = document.querySelector("#user-email");
+// var userPasswordSpan = document.querySelector("#user-password");
 
 
 // renderLastRegistered();
--- README1.md
+++ README1.md
@@ -1,1 +1,33 @@
 # scheduler-challenge
+
+## User Story
+
+```md
+AS AN employee with a busy schedule
+I WANT to add important events to a daily planner
+SO THAT I can manage my time effectively
+```
+
+## Acceptance Criteria
+
+```md
+GIVEN I am using a daily planner to create a schedule
+WHEN I open the planner
+THEN the current day is displayed at the top of the calendar✅
+WHEN I scroll down
+THEN I am presented with timeblocks for standard business hours✅
+WHEN I view the timeblocks for that day
+THEN each timeblock is color coded to indicate whether it is in the past, present, or future✅
+WHEN I click into a timeblock
+THEN I can enter an event✅
+WHEN I click the save button for that timeblock
+THEN the text for that event is saved in local storage❌
+WHEN I refresh the page
+THEN the saved events persist❌
+
+This was a deceptively difficult project that although involved taking bootstrap templates as opposed to building from scratch, still ended up giving me a tough time. I've realized that trying to completely emulate an app/ page is kind of like trying to mimic someones handwriting- very tricky if not close to impossible. I did however enjoy tinkering with all the different things that bootstrap can do with a page. Unfortunately, I spent way too much time being stuck on the design elements as well as getting my accurate time/date to work with moment.js, I ran out of time to complete the rest of the assignment: the local storage as well as getting the color blocks to correlate with the current time.
+
+Screenshots:
+./assets/SchedulerSS1.png
+./assets/SchedulerSS2.png
+./assets/SchedulerSS3.png
